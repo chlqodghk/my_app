e68c7061dd4ade6f2e78975845a6cb7124253963
chlqodghk <chlqodghk@nate.com>
2015-11-19 17:02:45 +0900

    add

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ var app = express();
 var path = require('path');
 var mongoose = require('mongoose');
 
+var bodyParser = require('body-parser');
+
 mongoose.connect(process.env.mongo_db);
 var db = mongoose.connection;
 db.once("open", function(){
@@ -40,9 +42,20 @@ Data.findOne({name:"myData"}, function(err, data){
 });
 
 
+var postSchema = mongoose.Schema({
+  title: {type:String, required:true},
+  body: {type:String, required:true},
+  createdAt: {type:Date, default:Date.now},
+  updatedAt: Date
+});
+var Post = mongoose.model('post', postSchema);
+
+
 app.set("view engine", 'ejs');
 //app.use(express.static(__dirname + '/public'));
 console.log(__dirname);
+
+app.use(bodyParser.json());
 /*
 app.get('/', function(req, res){
   res.render('my_first_ejs');
@@ -101,6 +114,57 @@ function getCounter(res){
     res.render('my_first_ejs', data);
   });
 }
+
+
+// set routes
+app.get('/posts', function(req, res){
+  Post.find({}).sort('-createdAt').exec(function(err, posts){
+    if(err){
+      return res.json({success:false, message:err});
+    }
+    //res.json({success:true, data:posts});
+    res.render("posts/index", {data:posts});
+  });
+}); // index
+app.post('/posts', function(req, res){
+  Post.create(req.body.post, function(err, posts){
+    if(err){
+      return res.json({success:false, message:err});
+    }
+    res.json({success:true, data:posts});
+  });
+}); // create
+
+app.get('/posts/:id', function(req, res){
+  Post.findById(req.params.id, function(err, post){
+    if(err){
+      return res.json({success:false, message:err});
+    }
+    res.json({success:true, data:post});
+  });
+}); // show
+
+app.put('/posts/:id', function(req, res){
+  req.body.post.updatedAt = Date.now();
+  Post.findByIdAndUpdate(req.params.id, req.body.post, function(err, post){
+    if(err){
+      return res.json({success:false, message:err});
+    }
+    res.json({success:true, message:post._id+" updated"});
+  });
+}); // update
+
+app.delete('/posts/:id', function(req, res){
+  Post.findByIdAndRemove(req.params.id, function(err, post){
+    if(err){
+      return res.json({success:false, message:err});
+    }
+    res.json({success:true, message:post._id+" deleted"});
+  });
+}); // delete
+
+
+
 
 app.listen(3000, function(){
   console.log('Server On!');
